dfc4cfb380f1f14b97afb929f2c0b4b3c69b6574
farady <farady@mailinator.com>
2015-07-17 12:30:18 +0300

    added about view to match about route.

---
+++ app/views/gallery/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>About</h1>
+        <hr class="featurette-divider">
+        <p>
+            This is gallery about page!
+        </p>
+    </div>
+</div>
+@stop
+
