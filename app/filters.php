844e6ac85f28531e4a85dbf57d04cbdd9c2a7228
farady <farady@mailinator.com>
2015-07-25 13:28:51 +0300

    Csrf filter created.

--- app/filters.php
+++ app/filters.php
@@ -1,9 +1,20 @@
 <?php
 
+use Symfony\Component\Security\Core\Util\StringUtils;
+use Illuminate\Session\TokenMismatchException;
+
 Route::filter('auth', function()
 {
 	if (!Auth::check())
         {
             return Redirect::to('/login');
         }
+});
+
+Route::filter('csrf', function() 
+{
+	if ( ! StringUtils::equals(Session::token(), Input::get('_token')))
+	{
+		throw new TokenMismatchException;
+	}
 });
